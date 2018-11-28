c765ecc50eee13369502b17b6b7a315ef42466db
Pedro Estacio <pedronetoestacio@gmail.com>
2018-11-28 21:24:08 -0200

    Concessionaria List
    OK

--- cf-interface-dev/src/app/paginas/concessionaria/concessionaria-lista/concessionaria-lista.component.ts
+++ cf-interface-dev/src/app/paginas/concessionaria/concessionaria-lista/concessionaria-lista.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ConcessionariasService} from '../../../../services/concessionarias.service';
+import {IConcessionaria} from '../../../../contracts/IConcessionaria';
 
 @Component({
   selector: 'app-concessionaria-lista',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConcessionariaListaComponent implements OnInit {
 
-  constructor() { }
+    titulo = 'concessionárias';
+    concessionarias: IConcessionaria[] = [];
+
+  constructor(public concessionarisService: ConcessionariasService) { }
 
   ngOnInit() {
+    this.carregarConcessionarias();
   }
+
+    carregarConcessionarias() {
+        this.concessionarisService.listar()
+            .subscribe(resposta => {
+                this.concessionarias = resposta;
+            });
+    }
+
+    deletarConcessionaria(id: number) {
+        this.concessionarisService.remover(id)
+            .subscribe(
+                sucesso => {
+                    this.carregarConcessionarias();
+                    console.log('Concessionaria OK');
+                    this.reloadView();
+                },
+                erro => {
+                    console.log('Concessionaria OK');
+                    setTimeout( () => {
+                        window.location.reload();
+                    }, 3500 );
+                }
+            );
+    }
+
+    reloadView() {
+        setTimeout( () => {
+            window.location.reload();
+        }, 1500 );
+    }
+
 
 }
